a1a49dbe25e37de9b2264c8c0ed0c5b7a05963a2
Kgothatso <k.seboya@gmail.com>
2025-05-11 16:31:46 +0200

    Apply module: angular-core

---
+++ src/main/webapp/app/app.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'jhi-root',
+  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterModule, MatMenuModule, MatToolbarModule, MatIconModule, MatButtonModule, NgOptimizedImage],
+  standalone: true,
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit {
+  appName = signal('');
+
+  ngOnInit(): void {
+    this.appName.set('bookServiceApp');
+  }
+}
---
+++ src/main/webapp/main.ts
@@ -0,0 +1,23 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { REMOVE_STYLES_ON_COMPONENT_DESTROY, bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.route';
+
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    importProvidersFrom([BrowserAnimationsModule]),
+    { provide: REMOVE_STYLES_ON_COMPONENT_DESTROY, useValue: true },
+  ],
+}).catch(err => console.error(err));
